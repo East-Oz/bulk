c69a939b24a27526f8c1f9008c0b238e1f9c34c4
Roman East-Oz <surdukov@gmail.com>
2019-02-11 13:00:29 +0300

    fix comments

--- bulk.cpp
+++ bulk.cpp
@@ -13,15 +13,17 @@ int gRowCount = 1;
 class Observer
 {
 public:
-	virtual void execute( std::vector<std::string>* ) = 0;
+	virtual void execute( std::vector<std::string>*, time_t* ) = 0;
+	virtual ~Observer() = default;
 };
 
 class Executor
 {
 public:
-	Executor(){};
+	Executor(): m_fct(time(0)){};
 
 	std::vector<std::string> m_commands;
+	time_t m_fct; // first command time
 private:
 	std::vector<std::shared_ptr<Observer>> m_subscribers;
 
@@ -31,9 +33,10 @@ public:
 		m_subscribers.push_back( ptrObs );
 	}
 
-	void set_commands( std::vector<std::string> commands )
+	void set_commands( std::vector<std::string> commands, time_t fct )
 	{
 		m_commands = commands;
+		m_fct = fct;
 		execute();
 	}
 
@@ -41,7 +44,7 @@ public:
 	{
 		for( auto s : m_subscribers )
 		{
-			s->execute( &m_commands );
+			s->execute( &m_commands, &m_fct );
 		}
 	}
 
@@ -56,34 +59,37 @@ public:
 		ptrExecutor->subscribe( wptr );
 	}
 
-	void execute( std::vector<std::string>* commands ) override
+	void execute( std::vector<std::string>* commands, time_t* fct ) override
 	{
-		time_t     now = time( 0 );
-		struct tm  tstruct;
-		char       buf[ 80 ];
-		tstruct = *localtime( &now );
-		strftime( buf, sizeof( buf ), "%OH%OM%OS", &tstruct );
-
-		auto time_now = std::chrono::system_clock::now();
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>( time_now.time_since_epoch() ) % 1000;
-
-		std::string fn = buf;
-		fn.append( std::to_string(ms.count()) );
-		fn.append( ".log" );
-
-		std::ofstream myfile;
-		myfile.open( fn );
-		if( myfile.is_open() )
+		if( commands->size() > 0 )
 		{
-			for( size_t i = 0; i < commands->size(); ++i )
+			struct tm  tstruct;
+			char       buf[ 80 ];
+			tstruct = *localtime( fct );
+			strftime( buf, sizeof( buf ), "%OH%OM%OS", &tstruct );
+
+			auto time_now = std::chrono::system_clock::now();
+			auto ms = std::chrono::duration_cast< std::chrono::milliseconds >(time_now.time_since_epoch()) % 1000;
+
+			std::string fn = "bulk";
+			fn.append( buf );
+			fn.append( std::to_string( ms.count() ) );
+			fn.append( ".log" );
+
+			std::ofstream myfile;
+			myfile.open( fn );
+			if( myfile.is_open() )
 			{
-				myfile << commands->at( i );
-				if(  i != ( commands->size() - 1 ) )
-					myfile << ", ";
-				else
-					myfile << std::endl;
+				for( size_t i = 0; i < commands->size(); ++i )
+				{
+					myfile << commands->at( i );
+					if( i != (commands->size() - 1) )
+						myfile << ", ";
+					else
+						myfile << std::endl;
+				}
+				myfile.close();
 			}
-			myfile.close();
 		}
 
 	}
@@ -98,17 +104,93 @@ public:
 		ptrExecutor->subscribe( wptr );
 	}
 
-	void execute( std::vector<std::string>* commands ) override
+	void execute( std::vector<std::string>* commands, time_t* fct ) override
 	{
-		for( size_t i = 0; i < commands->size(); ++i )
+		if( commands->size() > 0 )
 		{
-			std::cout << commands->at( i );
-			if( i < (commands->size() - 1) )
-				std::cout << ", ";
-			else
-				std::cout << std::endl;
+			std::cout << "bulk: ";
+			for( size_t i = 0; i < commands->size(); ++i )
+			{
+				std::cout << commands->at( i );
+				if( i < (commands->size() - 1) )
+					std::cout << ", ";
+				else
+					std::cout << std::endl;
+			}
 		}
 	}
+};
+
+class Parser
+{
+public:
+	Parser( std::shared_ptr<Executor> ptrExec ): m_pExecutor( ptrExec ) {};
+	void Start()
+	{
+		if( m_pExecutor == nullptr )
+		{
+			std::cout << "ERROR Executor is null! Return";
+			return;
+		}
+
+		int open_braces = 0;
+		bool is_ready_data = false;
+
+		std::vector<std::string> vector_str;
+		int count = 1;
+		time_t fct = time( 0 );
+		for( std::string line; std::getline( std::cin, line );)
+		{
+			if( count == 1 )
+			{
+				fct = time( 0 );
+			}
+
+			if( line.empty() ) // for exit sequence from console
+			{
+				is_ready_data = true;
+			}
+			else if( line.find( '{' ) != std::string::npos )
+			{
+				++open_braces;
+				if( open_braces == 1 )
+				{
+					is_ready_data = true;
+				}
+			}
+			else if( (line.find( '}' ) != std::string::npos) && (open_braces > 0) )
+			{
+				--open_braces;
+				if( open_braces == 0 )
+				{
+					is_ready_data = true;
+				}
+			}
+			else if( (count == gRowCount) && (open_braces == 0) )
+			{
+				vector_str.push_back( line );
+				is_ready_data = true;
+			}
+			else
+			{
+				vector_str.push_back( line );
+			}
+
+			if( is_ready_data )
+			{
+				m_pExecutor->set_commands( vector_str, fct );
+				vector_str.clear();
+				is_ready_data = false;
+				count = 0;
+			}
+			count++;
+		}
+		m_pExecutor->set_commands( vector_str, fct );
+		vector_str.clear();
+	}
+
+private:
+	std::shared_ptr<Executor> m_pExecutor;
 };
 
 int main( int argc, char *argv[] )
@@ -121,54 +203,8 @@ int main( int argc, char *argv[] )
 	FileObserver File( ptrExec );
 	ConsoleObserver Console( ptrExec );
 
-	int open_braces = 0;
-	bool is_ready_data = false;
-
-	std::vector<std::string> vector_str;
-	int count = 1;
-	for( std::string line; std::getline( std::cin, line );)
-	{
-		if( line.empty() )
-		{
-			is_ready_data = true;
-		}
-		else if (line.find('{') != std::string::npos)
-		{
-			++open_braces;
-			if( open_braces == 1 )
-			{
-				is_ready_data = true;
-			}
-		}
-		else if ((line.find('}') != std::string::npos) && (open_braces > 0))
-		{
-			--open_braces;
-			if (open_braces == 0)
-			{
-				is_ready_data = true;
-			}
-		}
-		else if( ( count == gRowCount ) && ( open_braces == 0 ) )
-		{
-			vector_str.push_back( line );
-			is_ready_data = true;
-		}
-		else
-		{
-			vector_str.push_back( line );
-		}
-
-		if( is_ready_data )
-		{
-			ptrExec->set_commands( vector_str );
-			vector_str.clear();
-			is_ready_data = false;
-			count = 0;
-		}
-		count++;
-	}
-	ptrExec->set_commands(vector_str);
-	vector_str.clear();
+	Parser ParserWorker( ptrExec );
+	ParserWorker.Start();
 
     return 0;
 }
